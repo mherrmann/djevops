b5092ee84f04ef5ae535cc65a2155d8387361b23
Michael Herrmann <michael@herrmann.io>
2025-12-31 10:48:16 +0100

    Restart Celery in update-server-from-git-repo.sh

--- bin/update-server-from-git-repo.sh
+++ bin/update-server-from-git-repo.sh
@@ -12,8 +12,16 @@ ${SCRIPT_DIR}/install-python-deps.sh
 ${SCRIPT_DIR}/migrate-db.sh
 ${SCRIPT_DIR}/collect-static-files.sh
 
+if supervisorctl status beat >/dev/null 2>&1; then
+  supervisorctl restart beat
+fi
+
+if supervisorctl status worker >/dev/null 2>&1; then
+  supervisorctl restart worker
+fi
+
 if supervisorctl status gunicorn >/dev/null 2>&1; then
-    supervisorctl status gunicorn | sed "s/.*[pid ]\([0-9]\+\)\,.*/\1/" | xargs kill -HUP
+  supervisorctl status gunicorn | sed "s/.*[pid ]\([0-9]\+\)\,.*/\1/" | xargs kill -HUP
 fi
 
 nginx -s reload
