e3b101c790fa356e05010efe378b7a938f102c0d
Michael Herrmann <michael@herrmann.io>
2026-01-18 13:34:30 +0100

    Fail install-python-deps.sh on error
    For example, when requirements.txt doesn't exist.

--- bin/install-python-deps.sh
+++ bin/install-python-deps.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-/srv/venv/bin/pip install -U pip | grep -v "^Requirement already satisfied" || true
-/srv/venv/bin/pip install -Ur /srv/app/requirements.txt | grep -v "^Requirement already satisfied" || true
+/srv/venv/bin/pip install -U pip | grep -v "^Requirement already satisfied"
+/srv/venv/bin/pip install -Ur /srv/app/requirements.txt | grep -v "^Requirement already satisfied"
