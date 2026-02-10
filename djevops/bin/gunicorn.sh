73852b59b2f361bb3324410a549689e030aac5af
Michael Herrmann <michael@herrmann.io>
2026-02-10 05:31:28 +0100

    Set gunicorn workers to the recommended number

--- djevops/bin/gunicorn.sh
+++ djevops/bin/gunicorn.sh
@@ -8,11 +8,13 @@ SOCKFILE=/var/run/django/$1.sock
 RUNDIR=$(dirname $SOCKFILE)
 test -d $RUNDIR || mkdir -p $RUNDIR
 
+# Set the number of workers to the recommended value of 2 * num_cores + 1.
+WORKERS=$(( 2 * $(nproc) + 1 ))
+
 DJANGO_WSGI_MODULE=$(find . -name wsgi.py | sed 's|\.py$||' | sed 's|^\./||' | sed 's|/|.|')
 
 exec gunicorn ${DJANGO_WSGI_MODULE}:application \
-  --workers 1 \
-  --threads 5 \
+  --workers $WORKERS \
   --bind=unix:$SOCKFILE \
   --log-level=warning \
   --log-file=-
