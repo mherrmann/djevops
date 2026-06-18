ce94b64c86c0351e2693aa71ff74048e0ea6da24
Michael Herrmann <michael@herrmann.io>
2026-06-18 06:59:18 +0200

    Fix a bug with S3 access

--- djevops/s3.py
+++ djevops/s3.py
@@ -2,6 +2,7 @@ from botocore.config import Config
 from botocore.exceptions import ClientError
 
 import boto3
+import botocore
 
 def upload(config, local_path, remote_path):
     client = get_client(config)
@@ -25,9 +26,24 @@ def get_client(config):
         endpoint_url=f'https://{endpoint}' if endpoint else None,
         aws_access_key_id=config['access-key-id'],
         aws_secret_access_key=config['secret-access-key'],
-        config=Config(s3={
+        config=_get_botocore_config(config)
+    )
+
+def _get_botocore_config(config):
+    kwargs = {
+        's3': {
             'addressing_style':
                 'path' if config.get('force-path-style') else 'auto',
             'payload_signing_enabled': config.get('sign-payload', False),
-        })
-    )
+        }
+    }
+    if _botocore_version() >= (1, 36):
+        # boto3 >= 1.36 sends CRC32 integrity checksums by default, which
+        # Ceph-based providers such as Linode reject with AccessDenied. The
+        # options that disable this only exist from 1.36 on.
+        kwargs['request_checksum_calculation'] = 'when_required'
+        kwargs['response_checksum_validation'] = 'when_required'
+    return Config(**kwargs)
+
+def _botocore_version():
+    return tuple(int(part) for part in botocore.__version__.split('.')[:2])
